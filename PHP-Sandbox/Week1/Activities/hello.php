a081608cee088ff717210e1d48936db2523f561f
Thomas Jackson <mrthomasjackson@fullsail.edu>
2015-10-01 19:41:51 -0400

    setup activity directory for week 1

---
+++ PHP-Sandbox/Week1/Activities/hello.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Hello</title>
+</head>
+<body>
+<?php
+echo "Hello World";
+echo "</br>";
+echo "Hello" . " World!";
+echo "</br>";
+echo 2 + 3;
+?>
+</body>
+</html>
